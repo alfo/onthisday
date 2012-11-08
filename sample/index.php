52d2e672c2c230f68613bb72b552176ff86650ab
Alex Forey <me@alexforey.com>
2012-11-08 20:44:47 +0000

    Sample is now a static resource from November 8

--- sample/index.php
+++ sample/index.php
@@ -1,16 +1,35 @@
-<?php
 
-	require('../functions.php');
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>On This Day</title>
+		<link rel="stylesheet" type="text/css" href="http://little.alexforey.com/onthisday/reset.css">
+		<link rel="stylesheet" type="text/css" href="http://little.alexforey.com/onthisday/style.css">
+	</head>
 
-	$items = onThisDay('bbc'); // Grab the items from Wikipedia
-	
+	<body>
+		<header>
+			<h1>On <em>This Day</em></h1>
+			<h3>In History</h3>
+			<h2>8 November</h2>
+		</header>
 
-	// -- Output HTML -- //
-	
-	makeHeader(); // Generate the header HTML
-
-	html($items); // Generate the HTML for the items
-
-	makeFooter(); // Generate the footer HTML
-
-?>
+		<section id="content">
+			<div class="item">
+				<h4>1912</h4>
+				<p>First Balkan War: The Greek Army accepted the surrender of the Ottoman garrison at Thessaloniki, preventing the Bulgarian Army from accepting it when they arrived one day later.</p>
+			</div>
+			<div class="item">
+				<h4>1940</h4>
+				<p>The Italian invasion of Greece failed as outnumbered Greek units repulsed the Italians in the Battle of Elaia–Kalamas.</p>
+			</div>
+			<div class="item">
+				<h4>1987</h4>
+				<p>A Provisional Irish Republican Army bomb exploded during a Remembrance Sunday ceremony  in Enniskillen, Northern Ireland, killing at least eleven people and injuring sixty-three others.</p>
+			</div>
+		</section>
+		<footer>
+			<h4>by Alex Forey</h4>
+		</footer>
+	</body>
+</html>
